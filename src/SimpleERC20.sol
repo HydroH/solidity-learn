f924a8b41ffa8d9982bf9294060293c5e3cc09e0
HydroH <ixlesis@gmail.com>
2025-07-10 14:05:28 +0800

    Fix allowance issue, optimize code

--- src/SimpleERC20.sol
+++ src/SimpleERC20.sol
@@ -2,21 +2,22 @@
 pragma solidity ^0.8.13;
 
 contract SimpleERC20 {
-    string private _name;
-    string private _symbol;
-    uint8 private _decimals;
-    uint256 private _totalSupply;
+    string name;
+    string symbol;
+    uint8 immutable decimals;
+    uint256 immutable totalSupply;
 
     mapping(address => uint256) private _balances;
     // Allowances mapping: owner => spender => amount
     mapping(address => mapping(address => uint256)) private _allowances;
 
     constructor(string memory name_, string memory symbol_, uint8 decimals_, uint256 totalSupply_) {
-        _name = name_;
-        _symbol = symbol_;
-        _decimals = decimals_;
-        _totalSupply = totalSupply_;
-        _balances[msg.sender] = totalSupply_; // Assign total supply to the contract creator
+        name = name_;
+        symbol = symbol_;
+        decimals = decimals_;
+        totalSupply = totalSupply_;
+        _balances[msg.sender] = totalSupply_;
+        emit Transfer(address(0), msg.sender, totalSupply_);
     }
 
     event Transfer(address indexed from, address indexed to, uint256 value);
@@ -25,40 +26,27 @@ contract SimpleERC20 {
     error InsufficientBalance(uint256 requested, uint256 available);
     error InsufficientAllowance(uint256 requested, uint256 available);
 
-    function name() public view returns (string memory) {
-        return _name;
-    }
-
-    function symbol() public view returns (string memory) {
-        return _symbol;
-    }
-
-    function decimals() public view returns (uint8) {
-        return _decimals;
-    }
-
-    function totalSupply() public view returns (uint256) {
-        return _totalSupply;
-    }
-
     function balanceOf(address owner) public view returns (uint256 balance) {
         return _balances[owner];
     }
 
     function transfer(address to, uint256 value) public returns (bool success) {
-        require(_balances[msg.sender] >= value, InsufficientBalance(value, _balances[msg.sender]));
-        _balances[msg.sender] -= value;
+        uint256 balance = _balances[msg.sender];
+        require(balance >= value, InsufficientBalance(value, balance));
+        _balances[msg.sender] = balance - value;
         _balances[to] += value;
         emit Transfer(msg.sender, to, value);
         return true;
     }
 
     function transferFrom(address from, address to, uint256 value) public returns (bool success) {
-        require(_allowances[from][to] >= value, InsufficientAllowance(value, _allowances[from][to]));
-        require(_balances[from] >= value, InsufficientBalance(value, _balances[from]));
+        uint256 allowance = _allowances[from][msg.sender];
+        require(allowance >= value, InsufficientAllowance(value, allowance));
+        uint256 balance = _balances[from];
+        require(balance >= value, InsufficientBalance(value, balance));
 
-        _allowances[from][to] -= value;
-        _balances[from] -= value;
+        _allowances[from][msg.sender] = allowance - value;
+        _balances[from] = balance - value;
         _balances[to] += value;
         emit Transfer(from, to, value);
         return true;
